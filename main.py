53e7a5402eba97fc97f39b384c6f83c87178ce29
Gianmarco Perini <gianmarco.perini@allievi.itsdigitalacademy.com>
2024-04-30 15:09:10 +0200

    comment

--- main.py
+++ main.py
@@ -45,31 +45,38 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+#Definisci la classe per la saml request
+class SAMLRequest:
+    def __init__(self, ID, Version, IssueInstant, Destination):
+        self.ID = ID
+        self.Version = Version
+        self.IssueInstant = IssueInstant
+        self.Destination = Destination
+
 #Funzione per la creazione della SAML request
 def createSAMLRequest():
     #Creazione dell'ID della SAML request
-    requestID = "_" + str(uuid.uuid4())
+    requestID = "_" + str(uuid.uuid4()) #Genera un ID univoco esempio: _f4f3d8f3-3b0d-4f6d-8b0f-2f3f4d3f4d3f
     #Creazione del timestamp
-    timestamp = datetime.datetime.now().isoformat()
+    timestamp = datetime.datetime.now().isoformat() #Genera un timestamp esempio: 2021-06-01T12:00:00.000000
     #Creazione del file XML
-    root = ET.Element("samlp:AuthnRequest", xmlns="urn:oasis:names:tc:SAML:2.0:protocol", ID=requestID, Version="2.0", IssueInstant=timestamp, Destination="http://localhost:5000/acs")
-    issuer = ET.SubElement(root, "saml:Issuer")
+    root = ET.Element("samlp:AuthnRequest", xmlns="urn:oasis:names:tc:SAML:2.0:protocol", ID=requestID, Version="2.0", IssueInstant=timestamp, Destination="http://localhost:5000/acs") #Crea il tag radice del file XML
+    issuer = ET.SubElement(root, "saml:Issuer") #Crea il tag figlio del tag radice
     issuer.text = "SP"
     #Creazione del file XML
-    tree = ET.ElementTree(root)
+    tree = ET.ElementTree(root) #Crea l'albero del file XML
     #Creazione del file XML
-    tree.write("SAMLRequest.xml")
+    tree.write("SAMLRequest.xml") #Scrive il file XML esempio: <samlp:AuthnRequest xmlns="urn:oasis:names:tc:SAML:2.0:protocol" ID="_f4f3d8f3-3b0d-4f6d-8b0f-2f3f4d3f4d3f" Version="2.0" IssueInstant="2021-06-01T12:00:00.000000" Destination="http://localhost:5000/acs"><saml:Issuer>SP</saml:Issuer></samlp:AuthnRequest>
     #Apertura del file XML
-    file = open("SAMLRequest.xml", "r")
+    file = open("SAMLRequest.xml", "r") #Apre il file XML 
     #Lettura del file XML
-    data = file.read()
+    data = file.read() #Legge il file XML
     #Codifica in base64
-    encoded = base64.b64encode(data.encode())
+    encoded = base64.b64encode(data.encode()) #Codifica il file XML in base64 esempio: 
     #Decodifica in stringa
-    decoded = encoded.decode()
+    decoded = encoded.decode() #Decodifica il file XML in stringa esempio: PHNhbWxwOkF1dGhuUmVxdWVzdCB4bWxucz0iaHR0cDovL2xvY2FsaG9zdDo1MDAwL2FjcyIgSUQ9Il9mNGYzZDhmMy0zYjBkLTRmNmQtOGIwZi0yZjNmNGQzZjRkM2YiIFZlcnNpb249IjIuMCIgSXNzdWluZ0luc3RhbnQ9IjIwMjEtMDYtMDFUMTI6MDA6MDAuMDAwMDAwIiBEZXN0aW5hdGlvbj0iaHR0cDovL2xvY2FsaG9zdDo1MDAwL2FjcyI+PHNhbWw6SXNzdWluZz5TUDwvc2FtbDpJc3N1aW5nPg==
     #Ritorno della stringa
     return decoded
-
 #Funzione per la creazione della SAML response
 
 def createSAMLResponse():
