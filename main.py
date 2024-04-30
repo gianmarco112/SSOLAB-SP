9af4c0ffa66c1ccb2d50eaf9cb98c5f0e163189d
Gianmarco Perini <gianmarco.perini@allievi.itsdigitalacademy.com>
2024-04-30 14:56:26 +0200

    Inizio

---
+++ main.py
@@ -0,0 +1,8 @@
+# SSOLAB-SP
+#Gruppo 2: SP
+#1. Deve accettare richieste di Auth dagli utenti e inviare loro la SAML request
+#2. Deve accettare di ritorno le SAML response e verificare assieme all’IdP associato la validità, 
+# chiedendo all’IdP se ha generato lui la risposta di certo ID a certo Time
+#3. Inviare la conferma di Auth all’utente (o fallimento)
+
+#API per l'interfacciamento
